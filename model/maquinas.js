4fe8aec4ffd4cf5de412626101a46d7f1db6a043
makyson <makyson10@gmail.com>
2023-12-18 17:42:06 -0300

    inicial

---
+++ model/maquinas.js
@@ -0,0 +1,37 @@
+
+
+const Sequelize = require('sequelize');
+const database = require('../db');
+
+
+const maquinaslist = database.define('maquinas',{
+  nome: {
+    type: Sequelize.STRING,
+    
+    allowNull: false,
+    
+    primaryKey: true
+   
+   
+     },
+
+
+
+   
+     
+     
+  
+     imageUrl: Sequelize.STRING,
+
+
+
+
+
+
+})
+//maquinaslist.sync({force: true});
+//maquinaslist.sync();
+
+
+
+module.exports = maquinaslist;
